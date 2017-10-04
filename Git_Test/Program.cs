bd5c7cf607472b361761f5598c546168130a4bea
Nathan Healy <S00169023@mail.itsligo.ie>
2017-10-04 16:39:44 +0100

    Test_Commit v1

---
+++ Git_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 5;
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("Git Test");
+               
+            }
+            
+        }
+    }
+}
